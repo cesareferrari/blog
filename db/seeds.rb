0e89b77a4e848f5d5480c15fef870fed1b8e76da
Cesare Ferrari <cesare@hey.com>
2023-11-27 20:48:26 -0500

    Add Devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.where(email: "cesare@hey.com").first_or_initialize
+user.update!(
+  password: "secret",
+  password_confirmation: "secret"
+)

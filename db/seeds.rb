fe4b9d88074a6d8e02e5a9764584f5473d1f35ab
Cesare Ferrari <cesare@hey.com>
2023-11-29 17:00:52 -0500

    Add styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,18 @@ user.update!(
   password: "secret",
   password_confirmation: "secret"
 )
+
+post1 = BlogPost.where(title: "Hello world").first_or_initialize
+post1.update!(
+  body: "This is my first post"
+)
+
+post2 = BlogPost.where(title: "Snowing today").first_or_initialize
+post2.update!(
+  body: "Lorem enim magnam natus officia dolorum? Ratione dolore at aliquam quam praesentium Mollitia magni dolor distinctio impedit repudiandae. Minima perferendis minima autem molestias dolorem. Repudiandae sed reiciendis blanditiis sunt sint"
+)
+
+post3 = BlogPost.where(title: "This is the news").first_or_initialize
+post3.update!(
+  body: "Adipisicing est sed commodi ex voluptates sed Nam quasi unde saepe mollitia quaerat. Ipsum obcaecati consectetur eveniet natus optio Suscipit amet labore doloribus quisquam nihil, qui facere Vitae autem tempora!"
+)
